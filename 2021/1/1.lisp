5d3c911581e1c0930abfb7cc36b9a52c8c6f5efd
Robert Smith <robert@stylewarning.com>
2021-11-30 23:01:10 -0800

    aoc 1 part 2

--- 2021/1/1.lisp
+++ 2021/1/1.lisp
@@ -4,22 +4,17 @@
 
 (cl:in-package #:aoc2021-cl)
 
-;;; use this if CL-LIST-TO-COALTON overflows stack
-(defun lisp-list-to-coalton-list (l)
-  ;; Unsafe if all of L aren't the same type!
-  (let ((cl coalton-library:nil))
-    (dolist (x (reverse l) cl)
-      (setf cl (coalton-library:Cons x cl)))))
-
+;;; Look ma, no Lisp!
 
 (cl:in-package #:aoc2021)
 
 (coalton-toplevel
+  ;; Part I
   (define aoc1-input-file
     (lisp String ()
       (cl:namestring
        (asdf:system-relative-pathname ':aoc "2021/1/1.input"))))
-  
+
   (declare read-file-into-string (String -> (Optional String)))
   (define (read-file-into-string filename)
     "Read the file named FILENAME into a string."
@@ -29,14 +24,14 @@
         (cl:if (cl:null contents)
                None
                (Some contents)))))
-  
+
   (declare split-string (Char -> String -> (List String)))
   (define (split-string c s)
     "Split the string S into substrings that are delineated by C."
     (lisp (List String) (c s)
       (cl-list-to-coalton
        (split-sequence:split-sequence c s))))
-  
+
   (declare parse-int-or-fail (String -> Integer))
   (define (parse-int-or-fail s)
     (fromSome "Failed to parse integer." (parse-int s)))
@@ -47,7 +42,7 @@
                           (read-file-into-string aoc1-input-file))))
       (map parse-int-or-fail
            (filter (/= "") (split-string #\Newline data)))))
-  
+
   (define-instance (Eq Ord)
     (define (== a b)
       (match (Tuple a b)
@@ -57,7 +52,7 @@
         (_             False)))
     (define (/= a b)
       (not (== a b))))
-  
+
   (declare countBy ((:a -> Boolean) -> (List :a) -> Integer))
   (define (countby f things)
     (let ((rec (fn (things count)
@@ -67,21 +62,57 @@
                                     (rec xs (+ 1 count))
                                     (rec xs count)))))))
       (rec things 0)))
-  
+
   (declare changes (Ord :a => ((List :a) -> (List Ord))))
   (define (changes things)
     (match things
       ((Nil) Nil)
       ((Cons _ tail)
        (zipWith <=> things tail))))
-  
+
   (declare count-increasing-pairs ((List Integer) -> Integer))
   (define (count-increasing-pairs ints)
     (countby (== LT) (changes ints)))
-  
-  (declare aoc1-solution (Unit -> Integer))
-  (define (aoc1-solution _)
-    (count-increasing-pairs (read-aoc-data))))
+
+  (declare aoc1.1-solution (Unit -> Integer))
+  (define (aoc1.1-solution _)
+    (count-increasing-pairs (read-aoc-data)))
+
+  ;; Part II
+  (declare cleave (Integer -> (List :a) -> (Tuple (List :a) (List :a))))
+  (define (cleave n xs)
+    (let ((rec (fn (n xs ys)
+                 (if (== 0 n)
+                     (Tuple (reverse ys) xs)
+                     (match xs
+                       ((Cons x xs) (rec (- n 1) xs (Cons x ys)))
+                       ((Nil) (rec 0 xs ys)))))))
+      (rec n xs Nil)))
+
+  (declare take (Integer -> (List :a) -> (List :a)))
+  (define (take n xs)
+    (fst (cleave n xs)))
+
+  (declare cdr ((List :a) -> (List :a)))
+  (define (cdr xs)
+    (match xs
+      ((Cons _ xs) xs)
+      ((Nil) Nil)))
+
+  (declare windows (Integer -> (List :a) -> (List (List :a))))
+  (define (windows n xs)
+    (let ((rec (fn (xs windows)
+                 (let ((head (take n xs)))
+                   (if (== n (length head))
+                       (rec (cdr xs) (cons head windows))
+                       (reverse windows))))))
+      (rec xs Nil)))
+
+  (declare aoc1.2-solution (Unit -> Integer))
+  (define (aoc1.2-solution _)
+    (count-increasing-pairs (map sum (windows 3 (read-aoc-data)))))
+
+)                                       ; COALTON-TOPLEVEL
 
 ;;; Pain points with Coalton:
 ;;;
@@ -91,7 +122,11 @@
 ;;;
 ;;; - Sloppy error handling.
 ;;;
-;;; - a FILTER bug bit me.
+;;; - a FILTER bug bit me (now fixed)
 ;;;
 ;;; - annoying to always type COALTON in the REPL
+;;;
+;;; - lots of missing list utilities
+;;;
+;;; - trailing whitespace from COALTON-TOPLEVEL
 
